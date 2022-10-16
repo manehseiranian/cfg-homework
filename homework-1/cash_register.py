8194d4faace60b94428c3982c2f5c50b53e17dd6
Maneh Seiranian <manehseiranian98@gmail.com>
2022-10-16 03:10:09 +0100

    Homework submission

--- homework-1/cash_register.py
+++ homework-1/cash_register.py
@@ -18,31 +18,69 @@ For example, method add_item probably accepts some kind of an item?..
 
 class CashRegister:
 
-    def __init__(self, total_items, total_price, discount):
-
-        self.total_items = None # {'item': 'price'}
+    def __init__(self):
+        self.total_items = dict()
         self.total_price = 0
         self.discount = 0
 
-    def add_item(self):
-        pass
+    def add_item(self, item, price):
+        self.total_items[item] = price
+        return self.total_items
 
-    def remove_item(self):
-        pass
+    def remove_item(self, item):
+        self.total_items.pop(item)
+        return self.total_items
 
-    def apply_discount(self):
-        pass
+    def apply_discount(self, item, discount_applicable, discount):
+        if discount_applicable:
+            discounted_price = self.total_items[item]*(1-discount/100)
+            print(f"{item}: £{'%.2f' % round(discounted_price,2)}")
+            self.total_items[item] = discounted_price
+        else:
+            print(f"{item}: £{'%.2f' % round(self.total_items[item],2)}")
+
 
     def get_total(self):
-        pass
+        print(f"The total cost is £{'%.2f' % round(sum(self.total_items.values()),2)}")
+
 
     def show_items(self):
-        pass
+        print(f"The following items are about to be purchased:")
+        for item in self.total_items:
+            print(item)
 
     def reset_register(self):
-        pass
+        self.total_items.clear()
 
 
 # EXAMPLE code run:
 
-# ADD
+list_of_items = CashRegister()
+
+# Adding items to the cash register:
+
+list_of_items.add_item('parmesan', 1.40)
+list_of_items.add_item('burrata', 3.15)
+list_of_items.add_item('grapes', 2.20)
+list_of_items.add_item('crackers', 1.60)
+
+# Removing an example item from the cash register:
+list_of_items.remove_item('grapes')
+
+# Applying a discount to discounted items:
+
+list_of_items.apply_discount('parmesan', True, 50)
+list_of_items.apply_discount('burrata', True, 30)
+list_of_items.apply_discount('crackers', False, None)
+
+# Showing the total cost of the purchase:
+list_of_items.get_total()
+
+# Viewing all the items in teh cash register:
+list_of_items.show_items()
+
+
+# removing all the items from the cash register:
+list_of_items.reset_register()
+
+list_of_items.show_items() # the view function is used once again to show that all the items have been deleted from the list
--- homework-1/student.py
+++ homework-1/student.py
@@ -1,3 +1,5 @@
+from statistics import mean
+import random
 """
 
 TASK 2
@@ -14,33 +16,77 @@ Software Student and Data Science student.
 
 class Student:
 
-    def __init__(self, name, surname, id):
+    def __init__(self, name, surname, age, student_id):
         self.name = name
         self.surname = surname
-        self.id = id
+        self.age = age
+        self.student_id = student_id
         self.subjects = dict()
-
-class CFGStudent(Student):
-    def __init__(self, name, surname, id, specialisation):
-        super().__init__(name, surname, id)
-        self.specialisation = specialisation #can be "Software"/"Data"/"Full Stack"
-
-
-class ClassTopics(CFGStudent): #Please note in this example the CFGD is made up of class topics such as SQL, Python, Dev Concepts, OOP, etc.
-    def __init__(self, name, surname, id, specialisation, topic):
-        super().__init__(name, surname, id, specialisation)
-        self.topic = topic
-
-
-class Marks(ClassTopics):
-    def __init__(self, name, surname, id, specialisation, topic, test_grade):
-        super().__init__(name, surname, id, specialisation, topic)
-        self.test_grade = test_grade
-
-    def average_mark:
 
 
 # class CFGStudent(<should inherit from Student>)
-#     create new methods that manage student's subects (add/remove new subject and its graade to the dict)
+#     create new method that manages student's subjects (add/remove new subject and its grade to the dict)
 #     create a method to view all subjects taken by a student
 #     create a method  (and a new variable) to get student's overall mark (use average)
+
+
+class CFGStudent(Student):
+    def __init__(self, name, surname, age, student_id, specialisation):
+        super().__init__(name, surname, age, student_id)
+        self.specialisation = specialisation
+        self.subjects = dict()
+
+    def add_subject(self, subject, mark):
+        self.subjects[subject] = mark
+        return self.subjects
+
+    def remove_subject(self, subject):
+        self.subjects.pop(subject)
+        return self.subjects
+
+    def view_subjects_taken(self):
+        print(f"{self.name} {self.surname} took the following classes:")
+        for subject in self.subjects:
+            print(subject)
+
+    def calculate_average_mark(self):
+        average_mark = round(mean(list(self.subjects.values())))
+        print(f"{self.name} {self.surname}'s average grade to date is {average_mark}%")
+
+
+
+# EXAMPLE code run:
+
+# Defining example students:
+anna = CFGStudent(name="Anna", surname="Jones", age=21, student_id="1234", specialisation="Software")
+sofie = CFGStudent(name="Sofie", surname="Kardashian", age=26, student_id="1235", specialisation="Software")
+michelle = CFGStudent(name="Michelle", surname="Heer", age=23, student_id="1236", specialisation="Data")
+
+# Adding example subjects and marks to students:
+
+subjects_software = ["SQL", "Python", "Testing", "OOP"]
+subjects_data = ["SQL", "Python", "Algorithms", "Big Data"]
+
+for i in range(0, len(subjects_software)):
+    anna.add_subject(subjects_software[i], random.randint(0, 100))
+    sofie.add_subject(subjects_software[i], random.randint(0, 100))
+
+for i in range(0, len(subjects_data)):
+    michelle.add_subject(subjects_data[i], random.randint(0, 100))
+
+# Removing example subjects from students
+
+michelle.remove_subject("Big Data")
+
+
+# Viewing the subjects taken by students
+
+anna.view_subjects_taken()
+sofie.view_subjects_taken()
+michelle.view_subjects_taken()
+
+
+# Viewing the overall grades of students:
+anna.calculate_average_mark()
+sofie.calculate_average_mark()
+michelle.calculate_average_mark()
